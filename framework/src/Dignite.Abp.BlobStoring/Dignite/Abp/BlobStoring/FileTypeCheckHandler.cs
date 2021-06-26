862907417b269a0b141667841b01996812b709fa
杜冠魁 <hello@dignite.com.cn>
2021-06-26 18:11:23 +0800

    refactor: change 'Mimedetective. Inmemory' package to 'Mime' package

--- framework/src/Dignite.Abp.BlobStoring/Dignite/Abp/BlobStoring/FileTypeCheckHandler.cs
+++ framework/src/Dignite.Abp.BlobStoring/Dignite/Abp/BlobStoring/FileTypeCheckHandler.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Threading.Tasks;
 using Volo.Abp;
-using MimeDetective.InMemory;
 
 namespace Dignite.Abp.BlobStoring
 {
@@ -19,10 +20,16 @@ namespace Dignite.Abp.BlobStoring
             // TODO: case sensitivity
             if (fileTypeCheckHandlerConfiguration.AllowedFileTypeNames != null && fileTypeCheckHandlerConfiguration.AllowedFileTypeNames.Length > 0)
             {
-                var fileType = context.BlobStream.DetectMimeType();
-                if (fileType != null)
+                string fileExtensionName = HeyRed.Mime.MimeGuesser.GuessExtension(context.BlobStream);
+
+                if (!fileExtensionName.IsNullOrEmpty())
                 {
-                    if (!fileTypeCheckHandlerConfiguration.AllowedFileTypeNames.Contains(fileType.Extension.EnsureStartsWith('.')))
+                    if (fileExtensionName.EnsureStartsWith('.').ToLower() == ".zip")
+                    {
+                        fileExtensionName = CheckForMsOfficeTypes(context.BlobStream);
+                    }
+
+                    if (!fileTypeCheckHandlerConfiguration.AllowedFileTypeNames.Contains(fileExtensionName.EnsureStartsWith('.')))
                     {
                         throw new BusinessException(
                             code: "Dignite.Abp.BlobStoring:010002",
@@ -43,5 +50,28 @@ namespace Dignite.Abp.BlobStoring
             return Task.CompletedTask;
         }
 
+        private static string CheckForMsOfficeTypes(Stream zip)
+        {
+            try
+            {
+                using (var zipFile = new ZipArchive(zip, ZipArchiveMode.Read,true))
+                {
+                    if (zipFile.Entries.Any(e => e.FullName.StartsWith("word/")))
+                        return ".docx";
+
+                    if (zipFile.Entries.Any(e => e.FullName.StartsWith("xl/")))
+                        return ".xlsx";
+
+                    if (zipFile.Entries.Any(e => e.FullName.StartsWith("ppt/")))
+                        return ".pptx";
+                }
+
+                return ".zip";
+            }
+            catch (InvalidDataException)
+            {
+                return null;  //ZIP archive can be corrupted
+            }
+        }
     }
 }
