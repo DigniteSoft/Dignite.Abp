9df33b2372d2aac1104f7a8189c5f90840716e8e
杜冠魁 <hello@dignite.com.cn>
2021-07-19 22:49:53 +0800

    refactor: rewrites the function of SignalRRealTimeNotifier

---
+++ framework/src/Dignite.Abp.Notifications.SignalR/Dignite/Abp/AspNetCore/SignalR/Notifications/SignalRRealTimeNotifier.cs
@@ -0,0 +1,32 @@
+﻿using Dignite.Abp.Notifications;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+
+namespace Dignite.Abp.AspNetCore.SignalR.Notifications
+{
+    public class SignalRRealTimeNotifier : IRealTimeNotifier, ITransientDependency
+    {
+        public ILogger Logger { get; set; }
+
+        private readonly IHubContext<NotificationHub,INotificationClient> _hubContext;
+
+        public SignalRRealTimeNotifier(
+        IHubContext<NotificationHub, INotificationClient> hubContext,
+        Logger<SignalRRealTimeNotifier> logger)
+        {
+            _hubContext = hubContext;
+            Logger = logger;
+        }
+
+
+        public async Task SendNotificationsAsync(UserNotificationInfo[] userNotifications)
+        {
+            await _hubContext.Clients.Users(
+                userNotifications.Select(un => un.UserId.ToString())
+                ).ReceiveNotifications();            
+        }
+    }
+}
