1430ecc95922fca1cf366413bbe498dbe907c688
杜冠魁 <hello@dignite.com.cn>
2021-07-23 13:28:32 +0800

    chore: add "Dignite.Abp.Settings" localization

---
+++ framework/src/Dignite.Abp.Settings/Dignite/Abp/Settings/Localization/DigniteAbpSettingsResource.cs
@@ -0,0 +1,9 @@
+﻿using Volo.Abp.Localization;
+
+namespace Dignite.Abp.Settings.Localization
+{
+    [LocalizationResourceName("DigniteAbpSettings")]
+    public class DigniteAbpSettingsResource
+    {
+    }
+}
