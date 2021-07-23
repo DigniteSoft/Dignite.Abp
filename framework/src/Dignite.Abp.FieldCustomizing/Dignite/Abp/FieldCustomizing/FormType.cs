5497677ac266cb9397757b8ba274021a20dcc151
杜冠魁 <hello@dignite.com.cn>
2021-07-23 12:28:58 +0800

    feat: add data dictionary form

--- framework/src/Dignite.Abp.FieldCustomizing/Dignite/Abp/FieldCustomizing/FormType.cs
+++ framework/src/Dignite.Abp.FieldCustomizing/Dignite/Abp/FieldCustomizing/FormType.cs
@@ -5,12 +5,12 @@ namespace Dignite.Abp.FieldCustomizing
     public enum FormType
     {
         /// <summary>
-        /// 
+        /// Simple type forms may not include other types of forms
         /// </summary>
         Simple,
 
         /// <summary>
-        /// 
+        /// Complex type forms can include simple type forms
         /// </summary>
         Complex
     }
---
+++ framework/src/Dignite.Abp.Settings/Dignite/Abp/Settings/SettingProviderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Dignite.Abp.FieldCustomizing.DataDictionaryForm;
+using JetBrains.Annotations;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Settings;
+
+namespace Dignite.Abp.Settings
+{
+    public static class SettingProviderExtensions
+    {
+        public static async Task<List<DataDictionary>> GetDataDictionariesAsync([NotNull] this ISettingProvider settingProvider, [NotNull] string name)
+        {
+            Check.NotNull(settingProvider, nameof(settingProvider));
+            Check.NotNull(name, nameof(name));
+
+            var dataDictionaries = JsonSerializer.Deserialize<List<DataDictionary>>(
+                await settingProvider.GetOrNullAsync(name)
+                );
+
+            return dataDictionaries;
+        }
+    }
+}
