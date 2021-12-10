c31edc68e480c0923fbaf1fdb5248398d6ba292b
BrettSun <sunbuchuan@hotmail.com>
2021-12-10 16:26:57 +0800

    demo

---
+++ demo/src/Dignite.Abp.Demo.Blazor/Models/LogModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dignite.Abp.Demo.Blazor.Models
+{
+    public class LogModel
+    {
+        public string Name { get; set; }
+
+        public int Content { get; set; }
+
+        public DateTime CreateTime { get; set; }
+    }
+}
