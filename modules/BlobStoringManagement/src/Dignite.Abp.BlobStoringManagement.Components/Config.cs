27485d31402e0c02cf3ddbc3166df7397f3084ea
BrettSun <sunbuchuan@hotmail.com>
2022-01-06 09:59:32 +0800

    上传组件

---
+++ modules/BlobStoringManagement/src/Dignite.Abp.BlobStoringManagement.Components/Config.cs
@@ -0,0 +1,122 @@
+﻿#region Using directives
+using Blazorise;
+using Blazorise.AntDesign;
+using Blazorise.Bootstrap;
+using Blazorise.Bootstrap5;
+using Blazorise.Modules;
+using Dignite.Abp.BlobStoringManagement.Components.Core;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+#endregion
+
+namespace Dignite.Abp.BlobStoringManagement.Components
+{
+    public static class Config
+    {
+
+        /// <summary>
+        /// Adds a ant design providers and component mappings.
+        /// </summary>
+        /// <param name="serviceCollection"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddDigniteAntDesignProviders(this IServiceCollection serviceCollection, Action<IClassProvider> configureClassProvider = null)
+        {
+            var classProvider = new AntDesignClassProvider();
+
+            configureClassProvider?.Invoke(classProvider);
+            serviceCollection.AddSingleton<IClassProvider>(classProvider);
+            serviceCollection.AddSingleton<IStyleProvider, AntDesignStyleProvider>();
+            serviceCollection.AddScoped<IThemeGenerator, AntDesignThemeGenerator>();
+            var components = Blazorise.AntDesign.Config.ComponentMap;
+            components[typeof(FileEditPlus)] = typeof(Components.Platform.AntDesign.FileEditPlus);
+            foreach (var mapping in components)
+            {
+                serviceCollection.AddTransient(mapping.Key, mapping.Value);
+            }
+
+            serviceCollection.AddScoped<IJSModalModule, Blazorise.AntDesign.Modules.AntDesignJSModalModule>();
+            serviceCollection.AddScoped<IJSTooltipModule, Blazorise.AntDesign.Modules.AntDesignJSTooltipModule>();
+
+            return serviceCollection;
+        }
+
+        /// <summary>
+        /// Adds a Bootstrap and component mappings.
+        /// </summary>
+        /// <param name="serviceCollection"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddDigniteBootstrapProviders(this IServiceCollection serviceCollection, Action<IClassProvider> configureClassProvider = null)
+        {
+            var classProvider = new Blazorise.Bootstrap.BootstrapClassProvider();
+
+            configureClassProvider?.Invoke(classProvider);
+            serviceCollection.AddSingleton<IClassProvider>(classProvider);
+            serviceCollection.AddSingleton<IStyleProvider, BootstrapStyleProvider>();
+            serviceCollection.AddScoped<IThemeGenerator, Blazorise.Bootstrap.BootstrapThemeGenerator>();
+            var components = Blazorise.Bootstrap.Config.ComponentMap;
+            components[typeof(FileEditPlus)] = typeof(Components.Platform.Bootstrap.FileEditPlus);
+            foreach (var mapping in components)
+            {
+                serviceCollection.AddTransient(mapping.Key, mapping.Value);
+            }
+
+            serviceCollection.AddScoped<IJSModalModule, Blazorise.Bootstrap.Modules.BootstrapJSModalModule>();
+            serviceCollection.AddScoped<IJSTooltipModule, Blazorise.Bootstrap.Modules.BootstrapJSTooltipModule>();
+
+            return serviceCollection;
+        }
+
+        /// <summary>
+        /// Adds a BootStrap5 providers and component mappings.
+        /// </summary>
+        /// <param name="serviceCollection"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddDigniteBootStrap5Providers(this IServiceCollection serviceCollection, Action<IClassProvider> configureClassProvider = null)
+        {
+            var classProvider = new Blazorise.Bootstrap5.BootstrapClassProvider();
+
+            configureClassProvider?.Invoke(classProvider);
+            serviceCollection.AddSingleton<IClassProvider>(classProvider);
+            serviceCollection.AddSingleton<IStyleProvider, Bootstrap5StyleProvider>();
+            serviceCollection.AddScoped<IThemeGenerator, Blazorise.Bootstrap5.BootstrapThemeGenerator>();
+            var components = Blazorise.Bootstrap5.Config.ComponentMap;
+            components[typeof(FileEditPlus)] = typeof(Platform.Bootstrap5.FileEditPlus);
+            foreach (var mapping in components)
+            {
+                serviceCollection.AddTransient(mapping.Key, mapping.Value);
+            }
+
+            serviceCollection.AddScoped<IJSModalModule, Blazorise.Bootstrap5.Modules.BootstrapJSModalModule>();
+            serviceCollection.AddScoped<IJSTooltipModule, Blazorise.Bootstrap5.Modules.BootstrapJSTooltipModule>();
+
+            return serviceCollection;
+        }
+
+        /// <summary>
+        /// Adds a Material providers and component mappings.
+        /// </summary>
+        /// <param name="serviceCollection"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddDigniteMaterialProviders(this IServiceCollection serviceCollection, Action<IClassProvider> configureClassProvider = null)
+        {
+            var classProvider = new Blazorise.Material.MaterialClassProvider();
+
+            configureClassProvider?.Invoke(classProvider);
+            serviceCollection.AddSingleton<IClassProvider>(classProvider);
+            serviceCollection.AddSingleton<IStyleProvider, Blazorise.Material.MaterialStyleProvider>();
+            serviceCollection.AddScoped<IThemeGenerator, Blazorise.Material.MaterialThemeGenerator>();
+            var components = Blazorise.Material.Config.ComponentMap;
+            components[typeof(FileEditPlus)] = typeof(Components.Platform.Material.FileEditPlus);
+            foreach (var mapping in components)
+            {
+                serviceCollection.AddTransient(mapping.Key, mapping.Value);
+            }
+
+            serviceCollection.AddScoped<IJSModalModule, Blazorise.Material.Modules.MaterialJSModalModule>();
+            serviceCollection.AddScoped<IJSTooltipModule, Blazorise.Material.Modules.MaterialJSTooltipModule>();
+
+            return serviceCollection;
+        }
+
+    }
+}
---
+++ modules/BlobStoringManagement/src/Dignite.Abp.BlobStoringManagement.Components/Core/FileEditPlus.razor.cs
@@ -0,0 +1,88 @@
+﻿using Blazorise;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dignite.Abp.BlobStoringManagement.Components.Core
+{
+    public partial class FileEditPlus
+    {
+        string SelectedFilenamesString => string.Join(", ", SelectedFileNames);
+        bool HasSelectedFileNames => SelectedFileNames.Any();
+
+        /// <summary>
+        /// 按钮样式
+        /// </summary>
+        [Parameter]
+        public RenderFragment? Content { get; set; }
+
+        public string FileName { get; set; }
+        public double Percentage { get; set; }
+
+        public EventCallbackFactory EventFactory { get; set; }
+
+        public List<SaveBytesInput> Files { get; set; } = new List<SaveBytesInput>();
+
+        public FileEditPlus()
+        {
+            EventFactory = new EventCallbackFactory();
+            Changed = EventFactory.Create<FileChangedEventArgs>(this, OnChanged);
+            Progressed = EventFactory.Create<FileProgressedEventArgs>(this, OnProgressed);
+        }
+
+        protected override Task OnInternalValueChanged(IFileEntry[] value)
+        {
+            return base.OnInternalValueChanged(value);
+        }
+
+        async Task OnChanged(FileChangedEventArgs e)
+        {
+            try
+            {
+                Files = new List<SaveBytesInput>();
+                foreach (var file in e.Files)
+                {
+                    FileName = file.Name;
+                    Percentage = 0;
+                    var stream1 = file.OpenReadStream(file.Size);
+                    var bytes = await stream1.GetAllBytesAsync();
+                    var fileInput = new SaveBytesInput
+                    {
+                        Bytes = bytes,
+                        EntityId = DateTime.Now.ToString("yyyyMMddHHmmssfff"),
+                        EntityType = "common-attachment",
+                        FileName = file.Name
+                    };
+                    Files.Add(fileInput);
+                }
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+            finally
+            {
+                this.StateHasChanged();
+            }
+        }
+        async Task OnProgressed(FileProgressedEventArgs e)
+        {
+            Percentage = e.Percentage;
+            Console.WriteLine($"File: {e.File.Name} Progress: {e.Percentage}");
+            this.StateHasChanged();
+        }
+
+        public async Task OnClick()
+        {
+            Console.WriteLine($"开始上传...");
+            foreach (var item in Files)
+            {
+                var result = await BlobService.SaveAsync("upload", item);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
