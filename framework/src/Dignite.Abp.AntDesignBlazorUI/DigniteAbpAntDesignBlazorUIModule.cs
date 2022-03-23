be3cf85a0aaf6c7cffe49cc85216921b18cb6e53
杜冠魁 <51013007+duguankui@users.noreply.github.com>
2022-03-23 09:45:57 +0800

    developing

--- framework/src/Dignite.Abp.AntDesignBlazorUI/DigniteAbpAntDesignBlazorUIModule.cs
+++ framework/src/Dignite.Abp.AntDesignBlazorUI/DigniteAbpAntDesignBlazorUIModule.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Dignite.Abp.BlazoriseUI;
+using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.BlazoriseUI;
 using Volo.Abp.Modularity;
 
 namespace Dignite.Abp.AntDesignBlazorUI
 {
     [DependsOn(
-        typeof(AbpBlazoriseUIModule)
+        typeof(AbpBlazoriseUIModule),
+        typeof(DigniteAbpBlazoriseUIModule)
         )]
     public class DigniteAbpAntDesignBlazorUIModule: AbpModule
     {
