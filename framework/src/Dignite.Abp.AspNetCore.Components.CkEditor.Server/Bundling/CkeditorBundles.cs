2d02368fdb2b93ec5da3eeb4ff7f55ccd2b96968
杜冠魁 <51013007+duguankui@users.noreply.github.com>
2022-06-11 09:59:07 +0800

    developing

---
+++ framework/src/Dignite.Abp.AspNetCore.Components.CkEditor.Server/Bundling/CkeditorBundles.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Dignite.Abp.AspNetCore.Components.CkEditor.Server.Bundling
+{
+    public class CkeditorBundles
+    {
+        public static class Scripts
+        {
+            public static string Ckeditor = "Ckeditor";
+        }
+    }
+}
