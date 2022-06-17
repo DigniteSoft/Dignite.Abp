5fce6b1fdc5906967da7d18bc6fb1d1a867ea1a2
杜冠魁 <51013007+duguankui@users.noreply.github.com>
2022-06-17 08:56:20 +0800

    Update FieldProviderSelector.cs

--- framework/src/Dignite.Abp.FieldCustomizing/Dignite/Abp/FieldCustomizing/Fields/FieldProviderSelector.cs
+++ framework/src/Dignite.Abp.FieldCustomizing/Dignite/Abp/FieldCustomizing/Fields/FieldProviderSelector.cs
@@ -17,18 +17,18 @@ namespace Dignite.Abp.FieldCustomizing.Fields
         }
 
         [NotNull]
-        public virtual IFieldProvider Get([NotNull] string controlProviderName)
+        public virtual IFieldProvider Get([NotNull] string providerName)
         {
             if (!FieldProviders.Any())
             {
                 throw new AbpException("No field control provider was registered! At least one provider must be registered to be able to use the field customizing system.");
             }
 
-            var fieldProvider = FieldProviders.SingleOrDefault(fp => fp.Name == controlProviderName);
+            var fieldProvider = FieldProviders.SingleOrDefault(fp => fp.Name == providerName);
 
             if (fieldProvider == null)
                 throw new AbpException(
-                    $"Could not find the field control provider with the name ({controlProviderName}) ."
+                    $"Could not find the field control provider with the name ({providerName}) ."
                 );
             else
                 return fieldProvider;
