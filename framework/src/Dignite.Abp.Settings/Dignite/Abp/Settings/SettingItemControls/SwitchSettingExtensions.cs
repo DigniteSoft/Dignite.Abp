9213f74f9b6fe9b988eaa5364be4ac84e3ff8f9a
杜冠魁 <51013007+duguankui@users.noreply.github.com>
2022-06-18 14:39:32 +0800

    Create SwitchSettingExtensions.cs

---
+++ framework/src/Dignite.Abp.Settings/Dignite/Abp/Settings/SettingItemControls/SwitchSettingExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Dignite.Abp.FieldCustomizing.Fields;
+using Dignite.Abp.FieldCustomizing.Fields.Switch;
+using System;
+using Volo.Abp.Settings;
+
+namespace Dignite.Abp.Settings.SettingItemControls
+{
+    public static class SwitchSettingExtensions
+    {
+        public static SettingDefinition UseSwitch(
+            this SettingDefinition settingDefinition,
+            Action<SwitchConfiguration> textboxConfigureAction)
+        {
+            var configuration = new SwitchConfiguration(new FieldConfigurationDictionary());
+            textboxConfigureAction(configuration);
+
+            settingDefinition.WithProperty(SettingDefinitionPropertiesNames.ConfigurationName, configuration.GetConfiguration());
+            settingDefinition.WithProperty(SettingDefinitionPropertiesNames.ProviderName, SwitchFieldProvider.ProviderName);
+
+            return settingDefinition;
+        }
+    }
+}
