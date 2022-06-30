ac5a27671a6d019ab5a6903fc4dc484f8af5ec61
杜冠魁 <51013007+duguankui@users.noreply.github.com>
2022-06-30 13:07:01 +0800

    developing

--- modules/pure-theme/src/Dignite.Abp.AspNetCore.Components.Web.PureTheme/wwwroot/libs/abp/js/theme.js
+++ modules/pure-theme/src/Dignite.Abp.AspNetCore.Components.Web.PureTheme/wwwroot/libs/abp/js/theme.js
@@ -1,16 +1,1 @@
 ﻿
-//主菜单的切换
-window.mainMenuToggle = (isHaveChildMenus) => {
-    if (isHaveChildMenus === false) {
-        document.body.classList.add("no-side-navbar");
-    }
-    else {
-        document.body.classList.remove("no-side-navbar");
-    }
-};
-
-//左侧菜单显示切换
-window.sideNavToggle = () => {
-    var sideNavbar = document.getElementById('sideNavbar');
-    sideNavbar.classList.toggle("show");
-};
