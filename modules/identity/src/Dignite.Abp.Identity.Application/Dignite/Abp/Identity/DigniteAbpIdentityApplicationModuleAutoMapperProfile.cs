2a92443d571d1c295b423d7d799e195261cff92b
杜冠魁 <51013007+duguankui@users.noreply.github.com>
2022-07-15 15:37:06 +0800

    dev

--- modules/identity/src/Dignite.Abp.Identity.Application/Dignite/Abp/Identity/DigniteAbpIdentityApplicationModuleAutoMapperProfile.cs
+++ modules/identity/src/Dignite.Abp.Identity.Application/Dignite/Abp/Identity/DigniteAbpIdentityApplicationModuleAutoMapperProfile.cs
@@ -8,19 +8,19 @@ namespace Dignite.Abp.Identity
         public DigniteAbpIdentityApplicationModuleAutoMapperProfile()
         {
             CreateMap<IdentityRole, Volo.Abp.Identity.IdentityRoleDto>()
-                .MapExtraProperties(definitionChecks: Volo.Abp.ObjectExtending.MappingPropertyDefinitionChecks.None);
+                .MapExtraProperties(definitionChecks: Volo.Abp.ObjectExtending.MappingPropertyDefinitionChecks.Source);
 
             CreateMap<IdentityRole, IdentityRoleDto>()
                 .ForMember(r => r.ParentId, r => r.Ignore())
                 .ForMember(r => r.Children, r => r.Ignore())
-                .MapExtraProperties(definitionChecks: Volo.Abp.ObjectExtending.MappingPropertyDefinitionChecks.None);
+                .MapExtraProperties(definitionChecks: Volo.Abp.ObjectExtending.MappingPropertyDefinitionChecks.Source);
 
             CreateMap<OrganizationUnit, OrganizationUnitDto>()
                 .ForMember(r => r.IsActive, r => r.Ignore())
                 .ForMember(r => r.Sort, r => r.Ignore())
                 .ForMember(r => r.Children, r => r.Ignore())
                 .ForMember(r => r.HasChild, r => r.Ignore())
-                .MapExtraProperties();
+                .MapExtraProperties(definitionChecks: Volo.Abp.ObjectExtending.MappingPropertyDefinitionChecks.Source);
 
             CreateMap<OrganizationUnitRole, OrganizationUnitRoleDto>();
         }
